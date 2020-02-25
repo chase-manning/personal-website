3903954d7ca746779d3fbda4506c7fd5b9318cd5
Chase Manning <chase@sharpcurrent.com>
2020-02-25 22:00:14 +1300

    First SVG Animation!

---
+++ src/app/home/circle.ts
@@ -0,0 +1,13 @@
+export class Circle {
+    radius: number;
+    x: number;
+    y: number;
+    fill: string;
+
+    constructor(radius: number, x: number, y: number, fill: string){
+        this.radius = radius;
+        this.x = x;
+        this.y = y;
+        this.fill = fill;
+    }
+}
