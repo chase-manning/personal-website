a43edbf9808590544c4c06d7bf0e48703aa40c4d
chase-manning <me@chasemanning.co.nz>
2020-02-25 22:00:14 +1300

    First SVG Animation!

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Circle } from './circle';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  private circles: Circle[] = [];
+
+  constructor() { 
+    this.circles.push(new Circle(100, 425, 475, '#000'));
+  }
 
   ngOnInit() {
   }
