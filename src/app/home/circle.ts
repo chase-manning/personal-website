adcb492dcf61c40c9b206cc377f0f5993c5129a1
chase-manning <me@chasemanning.co.nz>
2020-03-08 12:59:46 +1300

    Dynamically Generate Paths

--- src/app/home/circle.ts
+++ src/app/home/circle.ts
@@ -1,13 +1,27 @@
 export class Circle {
+    baseCircleRadius: number = 135;
+    baseCircleX: number = 245;
+    baseCircleY: number = 145;
+
     radius: number;
     x: number;
     y: number;
-    fill: string;
 
-    constructor(radius: number, x: number, y: number, fill: string){
+    get scale(): number {
+        return this.radius / this.baseCircleRadius;
+    }
+
+    get xTransform(): number {
+        return this.x - this.baseCircleX;
+    }
+
+    get yTransform(): number {
+        return this.y - this.baseCircleY;
+    }
+
+    constructor(radius: number, x: number, y: number){
         this.radius = radius;
         this.x = x;
         this.y = y;
-        this.fill = fill;
     }
 }
