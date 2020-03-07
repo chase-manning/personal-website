e1d504860c0203333699dab3b3ac31ef2df8d79a
Chase Manning <chase@sharpcurrent.com>
2020-03-08 12:59:46 +1300

    Dynamically Generate Paths

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,7 +11,7 @@ export class HomeComponent implements OnInit {
   private circles: Circle[] = [];
 
   constructor() { 
-    this.circles.push(new Circle(100, 425, 475, '#000'));
+    this.circles.push(new Circle(100, 100, 100));
   }
 
   ngOnInit() {
