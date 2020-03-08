38eee47c7f399db9a0f8e2463202b216a66077dd
Chase Manning <chase@sharpcurrent.com>
2020-03-08 19:03:50 +1300

    Fix issue with circles being the the wrong directory

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, HostListener } from '@angular/core';
-import { Circle } from '../loading/circle';
+import { Circle } from '../models/circle';
 
 @Component({
   selector: 'app-home',
