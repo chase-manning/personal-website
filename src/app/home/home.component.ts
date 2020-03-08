edc7c5971defc4b93ff83d1dbc86846d07ad24d6
Chase Manning <chase@sharpcurrent.com>
2020-03-08 13:11:42 +1300

    Generating mask dynamically

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,10 +11,24 @@ export class HomeComponent implements OnInit {
   private circles: Circle[] = [];
 
   constructor() { 
-    this.circles.push(new Circle(100, 100, 100));
+    this.innitialiseCircles();
   }
 
   ngOnInit() {
+  }
+
+  innitialiseCircles(): void {
+    let circleCount: number = Math.round(Math.random() * 5) + 10;
+
+    let i: number = 0;
+    while (i < circleCount){
+      // let radius: number = Math.round(Math.random() * 50) + 50;
+      let radius: number = 100;
+      let x: number = Math.round(Math.random() * (window.innerWidth/2 - 20)) + window.innerWidth/2;
+      let y: number = Math.round(Math.random() * (window.innerHeight - 20 - window.innerHeight/3)) + window.innerHeight/3;
+      this.circles.push(new Circle(radius, x, y));
+      i++;
+    }
   }
 
 }
