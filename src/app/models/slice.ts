f4a6c6abaa82dfc71d24d13304dd7bb2ac11b9f5
Chase Manning <chase@sharpcurrent.com>
2020-03-10 19:52:49 +1300

    Add fill to Slice

--- src/app/models/slice.ts
+++ src/app/models/slice.ts
@@ -1,8 +1,10 @@
 export class Slice {
     xTranslate: number;
     yTranslate: number;
+    fill: string;
 
     constructor(fill: string, xTranslate: number = 0, yTranslate: number = 0){
+        this.fill = fill;
         this.xTranslate = xTranslate;
         this.yTranslate = yTranslate;
     }
