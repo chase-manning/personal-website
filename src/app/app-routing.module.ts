a70464aae15cbaeff673641b5af5617ea3d6c601
Chase Manning <chase@sharpcurrent.com>
2020-03-16 18:38:36 +1300

    Add routing for new components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoadingComponent } from './components/loading/loading.component';
 import { HomeComponent } from './components/home/home.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ResumeComponent } from './resume/resume.component';
+import { ContactComponent } from './contact/contact.component';
 
 
 const routes: Routes = [
-  { path: '', component: HomeComponent}
+  { path: '', component: HomeComponent},
+  { path: 'portfolio', component: PortfolioComponent},
+  { path: 'resume', component: ResumeComponent},
+  { path: 'contact', component: ContactComponent},
 ];
 
 @NgModule({
