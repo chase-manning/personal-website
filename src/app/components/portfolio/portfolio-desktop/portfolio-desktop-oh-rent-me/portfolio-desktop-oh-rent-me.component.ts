e1748385dba165d7ca66717246ae36bb8193bb7d
chase-manning <me@chasemanning.co.nz>
2020-03-17 22:16:50 +1300

    Create Oh Rent Me Component

---
+++ src/app/components/portfolio/portfolio-desktop/portfolio-desktop-oh-rent-me/portfolio-desktop-oh-rent-me.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-portfolio-desktop-oh-rent-me',
+  templateUrl: './portfolio-desktop-oh-rent-me.component.html',
+  styleUrls: ['./portfolio-desktop-oh-rent-me.component.css']
+})
+export class PortfolioDesktopOhRentMeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
