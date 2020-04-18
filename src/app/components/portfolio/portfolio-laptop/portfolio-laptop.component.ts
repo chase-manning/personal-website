e55d3d8dca73385a0a99aa05227c8434d3fdbb9f
Chase Manning <chase@sharpcurrent.com>
2020-04-18 14:30:47 +1200

    Added Portfolio Laptop Component

---
+++ src/app/components/portfolio/portfolio-laptop/portfolio-laptop.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-portfolio-laptop',
+  templateUrl: './portfolio-laptop.component.html',
+  styleUrls: ['./portfolio-laptop.component.css']
+})
+export class PortfolioLaptopComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
