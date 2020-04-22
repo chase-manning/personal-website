1bc0f4853aa425be2358b83a2c1d641bef795967
chase-manning <me@chasemanning.co.nz>
2020-04-22 20:38:11 +1200

    Create Layer Component

---
+++ src/app/components/layer/layer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-layer',
+  templateUrl: './layer.component.html',
+  styleUrls: ['./layer.component.css']
+})
+export class LayerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
