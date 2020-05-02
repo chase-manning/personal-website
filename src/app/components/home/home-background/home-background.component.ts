3efa9cef8f5c5a4a1c3dde5f7a252b62af47559a
chase-manning <me@chasemanning.co.nz>
2020-05-02 16:49:39 +1200

    Remove Mouse Tracking Feature

--- src/app/components/home/home-background/home-background.component.ts
+++ src/app/components/home/home-background/home-background.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, HostListener } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home-background',
@@ -8,17 +8,7 @@ import { Component, OnInit, HostListener } from '@angular/core';
 export class HomeBackgroundComponent implements OnInit {
 
   constructor() { }
-  
-  @HostListener('document:mousemove', ['$event']) 
-  onMouseMove(event: MouseEvent) {
-    this.マウスX乗数 = (event.clientX / window.innerWidth) * -2 + 1;
-    this.マウスY乗数 = (event.clientY / window.innerHeight) * -2 + 1;
-  }
 
-  public マウスX乗数: number = 0.5;
-  public マウスY乗数: number = 0.5;
-
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
 }
