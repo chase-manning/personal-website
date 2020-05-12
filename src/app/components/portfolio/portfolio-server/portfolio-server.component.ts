3f2216f07f3817050018ccb58561a5fe2937a1c6
chase-manning <me@chasemanning.co.nz>
2020-05-12 21:44:47 +1200

    Create new Portfolio Server Component

---
+++ src/app/components/portfolio/portfolio-server/portfolio-server.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-portfolio-server',
+  templateUrl: './portfolio-server.component.html',
+  styleUrls: ['./portfolio-server.component.css']
+})
+export class PortfolioServerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
