cf4e5aeee1bdbe47ea46d6900f768dec4846556d
Chase Manning <chase@sharpcurrent.com>
2020-06-01 17:05:49 +1200

    Add Second Layer to Server

--- src/app/components/portfolio/portfolio-server/portfolio-server.component.ts
+++ src/app/components/portfolio/portfolio-server/portfolio-server.component.ts
@@ -7,12 +7,18 @@ import { Component, OnInit } from "@angular/core";
 })
 export class PortfolioServerComponent implements OnInit {
   topSectionLayerCount: number = 60;
+  secoondSectionLayerCount: number = 21;
 
   topSectionLayers: number[] = [];
+  secondSectionLayers: number[] = [];
 
   constructor() {
     for (var i = 0; i < this.topSectionLayerCount; i++) {
       this.topSectionLayers.push(i);
+    }
+
+    for (var i = 0; i < this.secoondSectionLayerCount; i++) {
+      this.secondSectionLayers.push(i);
     }
   }
 
