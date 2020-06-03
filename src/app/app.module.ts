4520d12b7afd5325d36c724ff7ac0973234dba62
chase-manning <me@chasemanning.co.nz>
2020-06-03 18:20:13 +1200

    Add New Confirmation Email Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoadingComponent } from './components/loading/loading.component';
-import { HomeComponent } from './components/home/home.component';
-import { PortfolioComponent } from './components/portfolio/portfolio.component';
-import { ResumeComponent } from './components/resume/resume.component';
-import { ContactComponent } from './components/contact/contact.component';
-import { HeaderComponent } from './components/header/header.component';
-import { LaptopOhRentMeComponent } from './components/portfolio/portfolio-laptop/laptop-oh-rent-me/laptop-oh-rent-me.component';
-import { PortfolioMobileComponent } from './components/portfolio/portfolio-mobile/portfolio-mobile.component';
-import { PortfolioLaptopComponent } from './components/portfolio/portfolio-laptop/portfolio-laptop.component';
-import { HomeBackgroundComponent } from './components/home/home-background/home-background.component';
-import { PortfolioServerComponent } from './components/portfolio/portfolio-server/portfolio-server.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { LoadingComponent } from "./components/loading/loading.component";
+import { HomeComponent } from "./components/home/home.component";
+import { PortfolioComponent } from "./components/portfolio/portfolio.component";
+import { ResumeComponent } from "./components/resume/resume.component";
+import { ContactComponent } from "./components/contact/contact.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { LaptopOhRentMeComponent } from "./components/portfolio/portfolio-laptop/laptop-oh-rent-me/laptop-oh-rent-me.component";
+import { PortfolioMobileComponent } from "./components/portfolio/portfolio-mobile/portfolio-mobile.component";
+import { PortfolioLaptopComponent } from "./components/portfolio/portfolio-laptop/portfolio-laptop.component";
+import { HomeBackgroundComponent } from "./components/home/home-background/home-background.component";
+import { PortfolioServerComponent } from "./components/portfolio/portfolio-server/portfolio-server.component";
+import { ConfirmationEmailComponent } from "./components/contact/confimation-email/confirmation-email.component";
 
 @NgModule({
   declarations: [
@@ -29,12 +30,10 @@ import { PortfolioServerComponent } from './components/portfolio/portfolio-serve
     PortfolioLaptopComponent,
     HomeBackgroundComponent,
     PortfolioServerComponent,
+    ConfirmationEmailComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
