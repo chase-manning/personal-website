91dd3d29bba090f9ef4a7b088bd0b6266540059b
Chase Manning <chase@sharpcurrent.com>
2020-06-03 17:29:02 +1200

    Implement Confirmation Email Prototype

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
 
 @Component({
-  selector: 'app-contact',
-  templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  selector: "app-contact",
+  templateUrl: "./contact.component.html",
+  styleUrls: ["./contact.component.css"],
 })
 export class ContactComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  test(): void {
+    firebase.initializeApp({
+      apiKey: "AIzaSyDpw02MMAvMb-O6JJmmQnhmhGeMuGJRut0",
+      authDomain: "chase-manning-portfolio.firebaseapp.com",
+      projectId: "chase-manning-portfolio",
+    });
+
+    var db = firebase.firestore();
+
+    db.collection("contacts")
+      .add({
+        name: "Lovelace2",
+        message: "Hi there, you are so cool!",
+        email: "chase@sharpcurrent.com",
+        from: "chase@sharpcurrent.com",
+        subject: "Thanks for Messaging Me!",
+        html: "<b>Test HTML</b><br/><p style='color:red;'>Meow</p>",
+      })
+      .then(function (docRef) {
+        console.log("Document written with ID: ", docRef.id);
+      })
+      .catch(function (error) {
+        console.error("Error adding document: ", error);
+      });
   }
-
 }
