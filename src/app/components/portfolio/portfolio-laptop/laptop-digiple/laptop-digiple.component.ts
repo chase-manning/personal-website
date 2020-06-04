0e4b1dcc15da7834de558559b8969764c0422b37
Chase Manning <chase@sharpcurrent.com>
2020-06-04 21:35:44 +1200

    Create Digiple Component

---
+++ src/app/components/portfolio/portfolio-laptop/laptop-digiple/laptop-digiple.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-laptop-digiple',
+  templateUrl: './laptop-digiple.component.html',
+  styleUrls: ['./laptop-digiple.component.css']
+})
+export class LaptopDigipleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
