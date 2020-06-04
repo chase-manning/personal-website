609d9f5bc2d0e67404a3a6f3db31b70429993575
Chase Manning <chase@sharpcurrent.com>
2020-06-04 21:05:50 +1200

    Add Route to preview Confirmation Email

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,22 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { LoadingComponent } from './components/loading/loading.component';
-import { HomeComponent } from './components/home/home.component';
-import { PortfolioComponent } from './components/portfolio/portfolio.component';
-import { ResumeComponent } from './components/resume/resume.component';
-import { ContactComponent } from './components/contact/contact.component';
-
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { LoadingComponent } from "./components/loading/loading.component";
+import { HomeComponent } from "./components/home/home.component";
+import { PortfolioComponent } from "./components/portfolio/portfolio.component";
+import { ResumeComponent } from "./components/resume/resume.component";
+import { ContactComponent } from "./components/contact/contact.component";
+import { ConfirmationEmailComponent } from "./components/contact/confimation-email/confirmation-email.component";
 
 const routes: Routes = [
-  { path: '', component: HomeComponent},
-  { path: 'portfolio', component: PortfolioComponent},
-  { path: 'resume', component: ResumeComponent},
-  { path: 'contact', component: ContactComponent},
+  { path: "", component: HomeComponent },
+  { path: "portfolio", component: PortfolioComponent },
+  { path: "resume", component: ResumeComponent },
+  { path: "contact", component: ContactComponent },
+  { path: "confirmationemail", component: ConfirmationEmailComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
