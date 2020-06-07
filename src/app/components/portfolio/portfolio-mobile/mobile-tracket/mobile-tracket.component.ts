d68765500dde5166b1f5a2a66e3df06bba39e3d9
Chase Manning <chase@sharpcurrent.com>
2020-06-07 20:46:43 +1200

    Create Mobile Tracket Component

---
+++ src/app/components/portfolio/portfolio-mobile/mobile-tracket/mobile-tracket.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mobile-tracket',
+  templateUrl: './mobile-tracket.component.html',
+  styleUrls: ['./mobile-tracket.component.css']
+})
+export class MobileTracketComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
