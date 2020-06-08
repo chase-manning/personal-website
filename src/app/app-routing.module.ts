ff53b3eb041026f5d530de48e90bbc98e1810dc6
chase-manning <me@chasemanning.co.nz>
2020-06-08 21:40:19 +1200

    Remove Redundant Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,13 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
-import { LoadingComponent } from "./components/loading/loading.component";
 import { HomeComponent } from "./components/home/home.component";
 import { PortfolioComponent } from "./components/portfolio/portfolio.component";
-import { ResumeComponent } from "./components/resume/resume.component";
 import { ContactComponent } from "./components/contact/contact.component";
-import { ConfirmationEmailComponent } from "./components/contact/confimation-email/confirmation-email.component";
 
 const routes: Routes = [
   { path: "", component: HomeComponent },
   { path: "portfolio", component: PortfolioComponent },
-  { path: "resume", component: ResumeComponent },
   { path: "contact", component: ContactComponent },
-  { path: "confirmationemail", component: ConfirmationEmailComponent },
 ];
 
 @NgModule({
