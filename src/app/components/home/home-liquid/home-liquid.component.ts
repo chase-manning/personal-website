a50a41b465681805a74cde82be5f4bb02491c70f
chase-manning <me@chasemanning.co.nz>
2020-06-10 22:48:02 +1200

    Create Basic Layout and scaling of home liquid

--- src/app/components/home/home-liquid/home-liquid.component.ts
+++ src/app/components/home/home-liquid/home-liquid.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { HostListener } from "@angular/core";
 
 @Component({
-  selector: 'app-home-liquid',
-  templateUrl: './home-liquid.component.html',
-  styleUrls: ['./home-liquid.component.css']
+  selector: "app-home-liquid",
+  templateUrl: "./home-liquid.component.html",
+  styleUrls: ["./home-liquid.component.css"],
 })
 export class HomeLiquidComponent implements OnInit {
+  svgScale: number;
 
-  constructor() { }
-
-  ngOnInit() {
+  @HostListener("window:resize", ["$event"])
+  onResize(event?) {
+    this.svgScale = window.innerWidth / 580;
   }
 
+  constructor() {
+    this.svgScale = window.innerWidth / 580;
+  }
+
+  ngOnInit() {}
 }
