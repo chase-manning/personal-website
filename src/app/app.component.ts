d99c5e64a620c61a582836b62f4b388ff3a074e9
Chase Manning <chase@sharpcurrent.com>
2020-06-18 21:00:17 +1200

    Implement GA Tracking on all routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,19 @@
 import { Component } from "@angular/core";
+import { Router, NavigationEnd } from "@angular/router";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"],
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(public router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        gtag("config", "UA-169905580-1", {
+          page_path: event.urlAfterRedirects,
+        });
+      }
+    });
+  }
+}
