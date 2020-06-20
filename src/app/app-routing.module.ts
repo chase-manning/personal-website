01a09eac7b85c290c4dffe6e45fa60cad4cac956
Chase Manning <me@chasemanning.co.nz>
2020-06-20 20:29:15 +1200

    Create 404 Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { Routes, RouterModule } from "@angular/router";
 import { HomeComponent } from "./components/home/home.component";
 import { PortfolioComponent } from "./components/portfolio/portfolio.component";
 import { ContactComponent } from "./components/contact/contact.component";
+import { NotFoundComponent } from "./not-found/not-found.component";
 
 const routes: Routes = [
   { path: "", component: HomeComponent },
   { path: "portfolio", component: PortfolioComponent },
   { path: "contact", component: ContactComponent },
+  { path: "404", component: NotFoundComponent },
+  { path: "**", redirectTo: "/404" },
 ];
 
 @NgModule({
