6e5b8d088082df310014457dc8c5ae3d34f19d26
Chase Manning <me@chasemanning.co.nz>
2020-06-20 21:15:26 +1200

    Add Title and Description for 404 Page

--- src/app/not-found/not-found.component.ts
+++ src/app/not-found/not-found.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { Title, Meta } from "@angular/platform-browser";
 
 @Component({
   selector: "app-not-found",
@@ -6,9 +7,16 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./not-found.component.css"],
 })
 export class NotFoundComponent implements OnInit {
-  constructor() {}
+  constructor(private title: Title, private meta: Meta) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.title.setTitle("Page Not Found");
+    this.meta.updateTag({
+      name: "description",
+      content:
+        "Looks like you are a bit lost! It's okay you can go home now, or...",
+    });
+  }
 
   kittens(): void {
     window.open("https://www.youtube.com/watch?v=SB-qEYVdvXA&t=10s", "_blank");
