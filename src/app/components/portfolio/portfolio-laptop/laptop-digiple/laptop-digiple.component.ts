b91bc4403224a7bd4955f46b9c75ff15bbf12412
Chase Manning <me@chasemanning.co.nz>
2020-06-20 15:33:48 +1200

    Split up Digiple css into smaller files

--- src/app/components/portfolio/portfolio-laptop/laptop-digiple/laptop-digiple.component.ts
+++ src/app/components/portfolio/portfolio-laptop/laptop-digiple/laptop-digiple.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-laptop-digiple',
-  templateUrl: './laptop-digiple.component.html',
-  styleUrls: ['./laptop-digiple.component.css']
+  selector: "app-laptop-digiple",
+  templateUrl: "./laptop-digiple.component.html",
+  styleUrls: [
+    "./laptop-digiple.component.css",
+    "./laptop-digiple.component.css",
+  ],
 })
 export class LaptopDigipleComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
