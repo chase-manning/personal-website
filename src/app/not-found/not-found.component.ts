c06cdd54bca62ac9ab77c8fb8df9c2efac1b589a
Chase Manning <me@chasemanning.co.nz>
2020-06-20 21:12:49 +1200

    Finish basic look of 404 page

--- src/app/not-found/not-found.component.ts
+++ src/app/not-found/not-found.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-not-found',
-  templateUrl: './not-found.component.html',
-  styleUrls: ['./not-found.component.css']
+  selector: "app-not-found",
+  templateUrl: "./not-found.component.html",
+  styleUrls: ["./not-found.component.css"],
 })
 export class NotFoundComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  kittens(): void {
+    window.open("https://www.youtube.com/watch?v=SB-qEYVdvXA&t=10s", "_blank");
   }
-
 }
