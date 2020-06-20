36f861d9f4e3808c4a8dcf59eba7d36c7fb76250
Chase Manning <me@chasemanning.co.nz>
2020-06-20 18:45:04 +1200

    Add Title and Meta Description to Home Page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,26 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Title, Meta } from "@angular/platform-browser";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
 })
-
 export class HomeComponent implements OnInit {
-
   backgroundEnabled: boolean = false;
   loadingEnabled: boolean = true;
 
-  constructor() { }
+  constructor(private title: Title, private meta: Meta) {}
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.title.setTitle("Chase Manning Freelance Developer");
+    this.meta.updateTag({
+      name: "description",
+      content:
+        "Let's Create Something Incredible Together! I'm Chase, a Freelance Developer who can't wait to hear about your exciting project!",
+    });
+  }
 
   enableBackground(): void {
     this.backgroundEnabled = true;
   }
-  
+
   disableLoading(): void {
     this.loadingEnabled = false;
   }
-
 }
