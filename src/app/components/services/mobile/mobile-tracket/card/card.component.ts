57145caf38faada9cb560e09a24658cdc4583ec4
Chase Manning <me@chasemanning.co.nz>
2020-06-27 19:34:16 +1200

    Add Card Selection and Animations

--- src/app/components/services/mobile/mobile-tracket/card/card.component.ts
+++ src/app/components/services/mobile/mobile-tracket/card/card.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
+
+export class Card {
+  color1: string;
+  color2: string;
+  color3: string;
+  color4: string;
+  number: string;
+  balance: string;
+  expiry: string;
+}
 
 @Component({
-  selector: 'app-card',
-  templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  selector: "app-card",
+  templateUrl: "./card.component.html",
+  styleUrls: ["./card.component.css"],
 })
 export class CardComponent implements OnInit {
+  @Input() card: Card;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
