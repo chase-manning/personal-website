f1dade7d50f1fa04af4aa422dfdc9ef704248c49
Chase Manning <me@chasemanning.co.nz>
2020-06-27 21:59:07 +1200

    Added Transaction Row Component

---
+++ src/app/components/services/mobile/mobile-tracket/transaction/transaction-row/transaction-row.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-transaction-row',
+  templateUrl: './transaction-row.component.html',
+  styleUrls: ['./transaction-row.component.css']
+})
+export class TransactionRowComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
