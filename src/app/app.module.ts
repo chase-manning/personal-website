366a2785db7703b5a65bd16c18c80e390f7b6b69
Chase Manning <chase.manning@iag.co.nz>
2020-07-05 18:59:30 +1200

    Rework folder structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,21 @@ import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { HomeComponent } from "./components/home/home.component";
-import { ServicesComponent } from "./components/services/services.component";
-import { ContactComponent } from "./components/contact/contact.component";
-import { HeaderComponent } from "./components/header/header.component";
-import { MobileComponent } from "./components/services/mobile/mobile.component";
-import { LaptopComponent } from "./components/services/laptop/laptop.component";
-import { ServerComponent } from "./components/services/server/server.component";
-import { ConfirmationEmailComponent } from "./components/contact/confimation-email/confirmation-email.component";
-import { LaptopDigipleComponent } from "./components/services/laptop/laptop-digiple/laptop-digiple.component";
-import { MobileTracketComponent } from "./components/services/mobile/mobile-tracket/mobile-tracket.component";
-import { HomeLiquidComponent } from "./components/home/home-liquid/home-liquid.component";
-import { NotFoundComponent } from "./components/not-found/not-found.component";
-import { CardComponent } from "./components/services/mobile/mobile-tracket/card/card.component";
-import { TransactionComponent } from "./components/services/mobile/mobile-tracket/transaction/transaction.component";
-import { TransactionRowComponent } from "./components/services/mobile/mobile-tracket/transaction/transaction-row/transaction-row.component";
+import { HomeComponent } from "./components/pages/home/home.component";
+import { ServicesComponent } from "./components/pages/services/services.component";
+import { ContactComponent } from "./components/pages/contact/contact.component";
+import { HeaderComponent } from "./components/shared/header/header.component";
+import { MobileComponent } from "./components/pages/services/mobile/mobile.component";
+import { LaptopComponent } from "./components/pages/services/laptop/laptop.component";
+import { ServerComponent } from "./components/pages/services/server/server.component";
+import { ConfirmationEmailComponent } from "./components/pages/contact/confimation-email/confirmation-email.component";
+import { LaptopDigipleComponent } from "./components/pages/services/laptop/laptop-digiple/laptop-digiple.component";
+import { MobileTracketComponent } from "./components/pages/services/mobile/mobile-tracket/mobile-tracket.component";
+import { HomeLiquidComponent } from "./components/pages/home/home-liquid/home-liquid.component";
+import { NotFoundComponent } from "./components/pages/not-found/not-found.component";
+import { CardComponent } from "./components/pages/services/mobile/mobile-tracket/card/card.component";
+import { TransactionComponent } from "./components/pages/services/mobile/mobile-tracket/transaction/transaction.component";
+import { TransactionRowComponent } from "./components/pages/services/mobile/mobile-tracket/transaction/transaction-row/transaction-row.component";
 import { GoogleAnalyticsService } from "./services/google-analytics.service";
 
 @NgModule({
