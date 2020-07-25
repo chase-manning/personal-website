8f97c52e17078aca0200653337383fe7eafe8286
Chase Manning <me@chasemanning.co.nz>
2020-07-26 10:18:42 +1200

    Testing Request Parameters in Function

--- functions/index.js
+++ functions/index.js
@@ -3,5 +3,6 @@ const sgMail = require("@sendgrid/mail");
 
 exports.email = functions.https.onRequest((request, response) => {
   console.log("test");
+  console.log(request.body.email);
   response.send("test");
 });
