79964b44bd5a4e74118191db684782b50d36ae5e
Chase Manning <me@chasemanning.co.nz>
2020-07-25 20:04:38 +1200

    Update Email Function to take Input

--- functions/index.js
+++ functions/index.js
@@ -1,16 +1,23 @@
 const functions = require("firebase-functions");
 const sgMail = require("@sendgrid/mail");
 
-exports.contact = functions.https.onRequest((request, response) => {
-  functions.logger.info("Start Contact Function");
-  sgMail.setApiKey(functions.config().sendgrid.key);
-  const msg = {
-    to: "me@chasemanning.co.nz",
-    from: "test@example.com",
-    subject: "Sending with Twilio SendGrid is Fun",
-    text: "and easy to do anywhere, even with Node.js",
-    html: "<strong>and easy to do anywhere, even with Node.js</strong>",
-  };
-  sgMail.send(msg);
-  response.send("Message Sent!");
+exports.email = functions.https.onRequest((request, response) => {
+  return Promise.resolve()
+    .then(() => {
+      sgMail.setApiKey(functions.config().sendgrid.key);
+      const msg = {
+        to: request.email,
+        from: "me@chasemanning.co.nz",
+        subject: request.subject,
+        text: request.text,
+        html: request.html,
+      };
+      sgMail.send(msg);
+      console.log("wood");
+      return response.send("Message Sent!");
+    })
+    .catch((err) => {
+      console.error(err);
+      return Promise.reject(err);
+    });
 });
