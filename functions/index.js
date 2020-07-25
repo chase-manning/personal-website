dd6d7c0469370977c6d90ee56c5e5a9e566c0eb6
Chase Manning <me@chasemanning.co.nz>
2020-07-26 11:18:25 +1200

    Update Domain for Email Function

--- functions/index.js
+++ functions/index.js
@@ -2,14 +2,14 @@ const functions = require("firebase-functions");
 const cors = require("cors")({ origin: true });
 const mailgun = require("mailgun-js");
 
-const DOMAIN = functions.config().mailgun.domain;
+const DOMAIN = "chasemanning.co.nz";
 const api_key = functions.config().mailgun.key;
 const mg = mailgun({ apiKey: api_key, domain: DOMAIN });
 
 exports.email = functions.https.onRequest((req, res) => {
   console.log("Key: " + api_key);
   console.log("Domain: " + DOMAIN);
-  console.log("Email: " + req.body.emil);
+  console.log("Email: " + req.body.email);
   console.log("Subject: " + req.body.subject);
   console.log("Text: " + req.body.text);
   console.log("HTML: " + req.body.html);
