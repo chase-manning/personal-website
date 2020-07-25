fe25830c03a08749869c4055f8f3d227f47fcad3
Chase Manning <me@chasemanning.co.nz>
2020-07-25 17:26:10 +1200

    Add Email Test to Contact Function

--- functions/index.js
+++ functions/index.js
@@ -2,5 +2,15 @@ const functions = require("firebase-functions");
 
 exports.contact = functions.https.onRequest((request, response) => {
   functions.logger.info("Start Contact Function");
+  const sgMail = require("@sendgrid/mail");
+  sgMail.setApiKey(functions.config().sendgrid.key);
+  const msg = {
+    to: "me@chasemanning.co.nz",
+    from: "test@example.com",
+    subject: "Sending with Twilio SendGrid is Fun",
+    text: "and easy to do anywhere, even with Node.js",
+    html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+  };
+  sgMail.send(msg);
   response.send("Message Sent!");
 });
