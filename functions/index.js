05fb901c9a6d4bcb6ab05d061efee6d680b1c594
Chase Manning <me@chasemanning.co.nz>
2020-07-26 11:07:02 +1200

    Add Detailed Logging for Email Function

--- functions/index.js
+++ functions/index.js
@@ -7,6 +7,13 @@ const api_key = functions.config().mailgun.key;
 const mg = mailgun({ apiKey: api_key, domain: DOMAIN });
 
 exports.email = functions.https.onRequest((req, res) => {
+  console.log("Key: " + api_key);
+  console.log("Domain: " + DOMAIN);
+  console.log("Email: " + req.body.emil);
+  console.log("Subject: " + req.body.subject);
+  console.log("Text: " + req.body.text);
+  console.log("HTML: " + req.body.html);
+
   return cors(req, res, () => {
     const data = {
       from: "Chase Manning <me@chasemanning.co.nz>",
@@ -15,7 +22,10 @@ exports.email = functions.https.onRequest((req, res) => {
       message: req.body.text,
     };
     mg.messages().send(data, (error, body) => {
+      console.log("Body:");
       console.log(body);
+      console.log("Error:");
+      console.log(error);
       body
         ? res.status(200).send("Email Sent Successfullly !")
         : res.status(500).send(error);
