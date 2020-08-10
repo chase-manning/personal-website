c659ff9ed9f5103e8268d1e49f482ccf2a62eb62
Chase Manning <me@chasemanning.co.nz>
2020-08-11 10:38:50 +1200

    Fix Issue with Home Page Meta Data

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from "@angular/core";
-import { Title, Meta } from "@angular/platform-browser";
 import { HostListener } from "@angular/core";
 
 @Component({
@@ -8,14 +7,7 @@ import { HostListener } from "@angular/core";
   styleUrls: ["./home.component.css"],
 })
 export class HomeComponent implements OnInit {
-  constructor(private title: Title, private meta: Meta) {}
+  constructor() {}
 
-  ngOnInit() {
-    this.title.setTitle("Chase Manning Freelance Developer");
-    this.meta.updateTag({
-      name: "description",
-      content:
-        "Let's Create Something Incredible Together! I'm Chase, a Freelance Developer who can't wait to hear about your exciting project!",
-    });
-  }
+  ngOnInit() {}
 }
