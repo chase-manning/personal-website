272140386a9a646c626c89176c0d4cbb3a30b623
Chase Manning <me@chasemanning.co.nz>
2020-08-11 18:54:12 +1200

    Add Socials Tally From Competitors

--- src/app/components/pages/contact/socials/socials.component.ts
+++ src/app/components/pages/contact/socials/socials.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+
+/*
+ * Twitter: 32
+ * Linked In: 29
+ * Instagram: 29
+ * Facebook: 20
+ * Dribble: 19
+ * Github: 12
+ * Behance: 10
+ * Code Pen: 4
+ * Vimeo: 2
+ * Pintrest: 2
+ * Stack Overflow: 2
+ * SlideShare: 1
+ * Flickr: 1
+ * Telegram: 1
+ * Medium: 1
+ */
 
 @Component({
-  selector: 'app-socials',
-  templateUrl: './socials.component.html',
-  styleUrls: ['./socials.component.css']
+  selector: "app-socials",
+  templateUrl: "./socials.component.html",
+  styleUrls: ["./socials.component.css"],
 })
 export class SocialsComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
