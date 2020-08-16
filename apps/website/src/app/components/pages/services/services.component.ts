b547eec40374a3f3fe2236b6b690c7d89d4d9503
Chase Manning <me@chasemanning.co.nz>
2020-08-16 21:35:54 +1200

    Remove Redundant Logic From Services Component

--- apps/website/src/app/components/pages/services/services.component.ts
+++ apps/website/src/app/components/pages/services/services.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from "@angular/core";
-import { HostListener } from "@angular/core";
 
 @Component({
   selector: "app-services",
@@ -7,30 +6,7 @@ import { HostListener } from "@angular/core";
   styleUrls: ["./services.component.css"],
 })
 export class ServicesComponent implements OnInit {
-  laptopScale: number;
-  mobileScale: number;
-  serverScale: number;
-
-  @HostListener("window:resize", ["$event"])
-  onResize(): void {
-    this.setSvgScale();
-  }
-
   ngOnInit() {}
 
-  constructor() {
-    this.setSvgScale();
-  }
-
-  get isMobile(): boolean {
-    return window.innerWidth < 520;
-  }
-
-  setSvgScale(): void {
-    const targetLaptopWidth = this.isMobile
-      ? window.innerWidth - 32 * 2
-      : (window.innerWidth - 61 * 3) / 2;
-    this.laptopScale = (targetLaptopWidth - 5) / 724;
-    this.mobileScale = 0.85;
-  }
+  constructor() {}
 }
