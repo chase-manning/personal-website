5cec28b9ccb1a202552ace4c8da6b98ceb856a71
Chase Manning <me@chasemanning.co.nz>
2020-12-30 15:28:01 +1300

    :rocket: Add Content to Desktop Popup

--- website/src/components/PopupDesktop.tsx
+++ website/src/components/PopupDesktop.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import styled from "styled-components";
+import Social from "./Social";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
 
 const StyledPopupDesktop = styled.div`
   position: fixed;
@@ -11,6 +13,35 @@ const StyledPopupDesktop = styled.div`
   opacity: ${(props: Props) => (props.open ? "1" : "0")};
   z-index: ${(props: Props) => (props.open ? "0" : "-100")};
   transition: opacity 0.4s;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const ColoredPannel = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: var(--purple);
+`;
+
+const Pannel = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Items = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const MenuItem = styled.div`
+  font-size: 30px;
+
+  :hover {
+    color: var(--purple);
+  }
 `;
 
 type Props = {
@@ -18,7 +49,43 @@ type Props = {
 };
 
 const PopupDesktop = (props: Props) => {
-  return <StyledPopupDesktop open={props.open}></StyledPopupDesktop>;
+  return (
+    <StyledPopupDesktop open={props.open}>
+      <ColoredPannel></ColoredPannel>
+      <Pannel>
+        <Items>
+          <Social
+            icon={<LinkedInIcon />}
+            link={"https://www.linkedin.com/in/chasemanning/"}
+            text={"Linked In"}
+          />
+          <Social
+            icon={<LinkedInIcon />}
+            link={"https://www.linkedin.com/in/chasemanning/"}
+            text={"Linked In"}
+          />
+          <Social
+            icon={<LinkedInIcon />}
+            link={"https://www.linkedin.com/in/chasemanning/"}
+            text={"Linked In"}
+          />
+          <Social
+            icon={<LinkedInIcon />}
+            link={"https://www.linkedin.com/in/chasemanning/"}
+            text={"Linked In"}
+          />
+        </Items>
+      </Pannel>
+      <Pannel>
+        <Items>
+          <MenuItem>Home</MenuItem>
+          <MenuItem>Home</MenuItem>
+          <MenuItem>Home</MenuItem>
+          <MenuItem>Home</MenuItem>
+        </Items>
+      </Pannel>
+    </StyledPopupDesktop>
+  );
 };
 
 export default PopupDesktop;
