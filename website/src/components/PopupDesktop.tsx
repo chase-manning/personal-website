3b7e3abdb04d6f7f3733c4c1a835076d6be82c53
Chase Manning <me@chasemanning.co.nz>
2020-12-30 15:15:20 +1300

    :sparkles: Create Desktop Popup

---
+++ website/src/components/PopupDesktop.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledPopupDesktop = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: var(--bg);
+  opacity: ${(props: Props) => (props.open ? "1" : "0")};
+  z-index: ${(props: Props) => (props.open ? "0" : "-100")};
+  transition: opacity 0.2s;
+`;
+
+type Props = {
+  open: boolean;
+};
+
+const PopupDesktop = (props: Props) => {
+  return <StyledPopupDesktop open={props.open}></StyledPopupDesktop>;
+};
+
+export default PopupDesktop;
