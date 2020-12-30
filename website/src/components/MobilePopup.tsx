8dfc1eb8a447da8dbe8efd80fbb505559f15c34f
Chase Manning <me@chasemanning.co.nz>
2020-12-31 10:57:43 +1300

    :rocket: Add Illustration to Mobile Popup

--- website/src/components/MobilePopup.tsx
+++ website/src/components/MobilePopup.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import illustration from "../assets/images/bg-header-mobile.png";
 
 type PopupProps = {
   open: boolean;
@@ -9,12 +10,19 @@ const StyledMobilePopup = styled.div`
   position: fixed;
   top: 0;
   left: 0;
-  height: 100%;
+  height: calc(100% - 95px);
   width: 100%;
   margin-top: 95px;
   background-color: red;
   transform: translateX(${(props: PopupProps) => (props.open ? "0" : "100%")});
   transition: transform 0.3s;
+`;
+
+const Illustration = styled.img`
+  width: 100%;
+  position: absolute;
+  bottom: 0;
+  right: 0;
 `;
 
 type Props = {
@@ -23,7 +31,11 @@ type Props = {
 };
 
 const MobilePopup = (props: Props) => {
-  return <StyledMobilePopup open={props.open}>meow</StyledMobilePopup>;
+  return (
+    <StyledMobilePopup open={props.open}>
+      <Illustration src={illustration} />
+    </StyledMobilePopup>
+  );
 };
 
 export default MobilePopup;
