ee28d5a6d605bf569e00ef5faf48012f16b7f371
Chase Manning <me@chasemanning.co.nz>
2020-12-31 14:07:30 +1300

    :lipstick: Reformat Socials Layout on Mobile

--- website/src/components/Social.tsx
+++ website/src/components/Social.tsx
@@ -4,26 +4,34 @@ import styled from "styled-components";
 const StyledSocial = styled.a`
   cursor: pointer;
   display: flex;
+
   margin: 15px 0;
+  @media only screen and (max-width: 639px) {
+    margin: 7px 0;
+  }
 `;
 
 const Icon = styled.div`
-  margin-right: 40px;
   display: flex;
   align-items: center;
 
   color: var(--purple);
+  font-size: 18px;
+  margin-right: 40px;
   @media only screen and (max-width: 639px) {
     color: var(--sub);
+    font-size: 19px;
+    margin-right: 15px;
   }
 `;
 
 const Text = styled.p`
-  font-size: 18px;
   font-weight: 400;
 
+  font-size: 18px;
   @media only screen and (max-width: 639px) {
     color: var(--sub);
+    font-size: 14px;
   }
 `;
 
