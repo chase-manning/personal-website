723b144ed1dbb5593575196ae07d083024cb5ca6
Chase Manning <me@chasemanning.co.nz>
2022-01-01 15:39:29 +0000

    :rocket: add illustration to say hello

--- src/pages/portfolio/SayHello.tsx
+++ src/pages/portfolio/SayHello.tsx
@@ -1,12 +1,19 @@
+import { useRef } from "react";
 import styled from "styled-components";
+
 import LineHeader from "../../components/LineHeader";
 import Socials from "../../components/Socials";
+import getScrollPercent from "../../utils/scroll-percent";
+import SayHelloIllustration from "./SayHelloIllustration";
 
 const StyledSayHello = styled.div`
+  position: relative;
   width: 100%;
+  margin-top: 40px;
 `;
 
 const Container = styled.div`
+  position: relative;
   padding: 60px;
   background: var(--bg);
   border-radius: 48px;
@@ -20,13 +27,56 @@ const Bold = styled.p`
   margin-bottom: 70px;
 `;
 
-const SayHello = () => {
+const FooterExtension = styled.div`
+  width: 150vw;
+  height: 1000px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translateX(-50%) rotate(-20deg);
+  background: var(--dark-bg);
+`;
+
+const SecondLayer = styled.div`
+  position: absolute;
+  left: 50%;
+  bottom: -16px;
+  transform: translateX(-50%);
+  background: #203696;
+  border-radius: 34px;
+  height: 100%;
+  width: calc(100% - 48px);
+`;
+
+const ThirdLayer = styled.div`
+  position: absolute;
+  left: 50%;
+  bottom: -32px;
+  transform: translateX(-50%);
+  background: #1b1b87;
+  border-radius: 34px;
+  height: 100%;
+  width: calc(100% - 98px);
+`;
+
+interface Props {
+  scroll: number;
+}
+
+const SayHello = ({ scroll }: Props) => {
+  const pageRef = useRef<HTMLDivElement>(null);
+  const scrollPercent = getScrollPercent(pageRef.current, scroll);
+
   return (
-    <StyledSayHello>
+    <StyledSayHello ref={pageRef}>
+      <FooterExtension />
+      <ThirdLayer />
+      <SecondLayer />
       <Container>
         <LineHeader text="Say hello 👋" />
         <Bold>My Socials.</Bold>
         <Socials />
+        <SayHelloIllustration scrollPercent={scrollPercent} />
       </Container>
     </StyledSayHello>
   );
