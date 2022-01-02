0969a94955c9e37fa95bb2b60715bbb021bce10e
Chase Manning <me@chasemanning.co.nz>
2022-01-02 17:21:59 +0000

    :iphone: mobile home content view

--- src/pages/home/HomeContent.tsx
+++ src/pages/home/HomeContent.tsx
@@ -1,7 +1,10 @@
 import styled from "styled-components";
+
 import LineHeader from "../../components/LineHeader";
+import whiteSquare from "../../assets/hero/main-pic-9.png";
 
 const StyledHomeContent = styled.div`
+  position: relative;
   display: flex;
   flex-direction: column;
 
@@ -38,11 +41,52 @@ const Header = styled.h2`
   }
 `;
 
-const HomeContent = () => {
+const WhiteSquare = styled.img`
+  position: absolute;
+  top: 66%;
+  right: -46%;
+  width: 150px;
+  transition: all 0.2s;
+
+  @media only screen and (min-width: 640px) {
+    display: none;
+  }
+`;
+
+const BlurredSquare = styled.img`
+  position: absolute;
+  top: 20%;
+  right: -18%;
+  width: 100px;
+  filter: blur(2px);
+  transition: all 0.2s;
+
+  @media only screen and (min-width: 640px) {
+    display: none;
+  }
+`;
+
+interface Props {
+  scrollPercent: number;
+}
+
+const HomeContent = ({ scrollPercent }: Props) => {
   return (
     <StyledHomeContent>
       <LineHeader text="Chase Manning" />
       <Header>Hi, I'm Chase, your Friendly Neighborhood Developer.</Header>
+      <WhiteSquare
+        src={whiteSquare}
+        style={{
+          transform: `translateY(calc(${scrollPercent} * -100px))`,
+        }}
+      />
+      <BlurredSquare
+        src={whiteSquare}
+        style={{
+          transform: `translateY(calc(${scrollPercent} * 100px))`,
+        }}
+      />
     </StyledHomeContent>
   );
 };
