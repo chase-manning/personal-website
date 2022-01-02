f92252f2cf267a21fba0dd8efce88e9ee95d569b
Chase Manning <me@chasemanning.co.nz>
2022-01-02 17:21:52 +0000

    :iphone: mobile about me page view

--- src/pages/about-me/AboutMePage.tsx
+++ src/pages/about-me/AboutMePage.tsx
@@ -6,6 +6,7 @@ import arrow from "../../assets/ui/arrow.svg";
 import pinkSquare from "../../assets/hero/main-pic-8.png";
 import whiteSquare from "../../assets/hero/main-pic-9.png";
 import getScrollPercent from "../../utils/scroll-percent";
+import { useDevice } from "../../hooks/use-device";
 
 const StyledAboutMePage = styled.div`
   position: relative;
@@ -17,6 +18,9 @@ const StyledAboutMePage = styled.div`
   @media only screen and (max-width: 1400px) {
     height: 80vh;
   }
+  @media only screen and (max-width: 639px) {
+    height: auto;
+  }
 
   padding: 60px 0;
   padding-left: calc(15% + 60px);
@@ -27,7 +31,7 @@ const StyledAboutMePage = styled.div`
     padding-left: calc(5% + 35px);
   }
   @media only screen and (max-width: 639px) {
-    padding-left: 0;
+    padding: 60px 30px;
   }
 `;
 
@@ -45,14 +49,18 @@ const TopSection = styled.div`
 `;
 
 const Description = styled.p`
-  font-size: 42px;
   line-height: 1.3333;
   font-weight: 600;
-  max-width: 850px;
 
+  font-size: 42px;
+  max-width: 850px;
   @media only screen and (max-width: 1400px) {
     font-size: 32px;
     max-width: 650px;
+  }
+  @media only screen and (max-width: 639px) {
+    font-size: 20px;
+    max-width: 270px;
   }
 `;
 
@@ -69,6 +77,11 @@ const Bold = styled.p`
     max-width: 650px;
     margin-top: 30px;
   }
+  @media only screen and (max-width: 639px) {
+    font-size: 72px;
+    max-width: 550px;
+    margin-top: 130px;
+  }
 `;
 
 const Button = styled.button`
@@ -76,6 +89,7 @@ const Button = styled.button`
   border-radius: 20px;
   display: flex;
   align-items: center;
+  justify-content: center;
   cursor: pointer;
   margin-bottom: 60px;
 
@@ -92,6 +106,15 @@ const Button = styled.button`
     padding: 20px 40px;
     margin-bottom: 30px;
   }
+  @media only screen and (max-width: 639px) {
+    width: 100%;
+    margin-top: 3.3rem;
+  }
+`;
+
+const ButtonContent = styled.div`
+  display: flex;
+  align-items: center;
 `;
 
 const ButtonText = styled.div`
@@ -113,25 +136,40 @@ const Illustration = styled.div`
   position: absolute;
   top: 0;
   right: 0;
+  overflow: hidden;
+
   height: 100vh;
   width: 100vh;
-  overflow: hidden;
+  @media only screen and (max-width: 639px) {
+    height: 200vw;
+    width: 100vw;
+  }
 `;
 
 const PinkSquare = styled.img`
   position: absolute;
-  right: -34%;
-  top: 15%;
   width: 120%;
   transition: all 0.2s;
+
+  top: 15%;
+  right: -34%;
+  @media only screen and (max-width: 639px) {
+    top: 24%;
+    right: -44%;
+  }
 `;
 
 const WhiteSquare = styled.img`
   position: absolute;
-  right: 48%;
-  top: 85%;
   width: 30%;
   transition: all 0.2s;
+
+  top: 85%;
+  right: 48%;
+  @media only screen and (max-width: 639px) {
+    top: 68%;
+    right: -2%;
+  }
 `;
 
 const BlurredSquare = styled.img`
@@ -141,6 +179,10 @@ const BlurredSquare = styled.img`
   top: 4%;
   width: 18%;
   transition: all 0.2s;
+
+  @media only screen and (max-width: 639px) {
+    display: none;
+  }
 `;
 
 interface Props {
@@ -150,8 +192,7 @@ interface Props {
 const AboutMePage = ({ scroll }: Props) => {
   const pageRef = useRef<HTMLDivElement>(null);
   const scrollPercent = getScrollPercent(pageRef.current, scroll);
-
-  console.log(scrollPercent);
+  const { isMobile } = useDevice();
 
   return (
     <StyledAboutMePage ref={pageRef} id="about-me-scroll">
@@ -159,13 +200,17 @@ const AboutMePage = ({ scroll }: Props) => {
         <PinkSquare
           src={pinkSquare}
           style={{
-            transform: `translateY(calc(${scrollPercent} * -200px))`,
+            transform: `translateY(calc(${scrollPercent} * -${
+              isMobile ? 100 : 200
+            }px))`,
           }}
         />
         <WhiteSquare
           src={whiteSquare}
           style={{
-            transform: `translateY(calc(${scrollPercent} * -400px))`,
+            transform: `translateY(calc(${scrollPercent} * -${
+              isMobile ? 200 : 400
+            }px))`,
           }}
         />
         <BlurredSquare
@@ -191,8 +236,10 @@ const AboutMePage = ({ scroll }: Props) => {
                 .focus()
             }
           >
-            <ButtonText>View my GitHub</ButtonText>
-            <ButtonArrow src={arrow} />
+            <ButtonContent>
+              <ButtonText>View my GitHub</ButtonText>
+              <ButtonArrow src={arrow} />
+            </ButtonContent>
           </Button>
         </div>
       </Content>
