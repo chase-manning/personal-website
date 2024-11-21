2c96646cb2f0b6a6d0dfe5a5aad127b60a3b3c3a
Chase Manning <chase@manning.dev>
2024-11-21 21:33:22 +0000

    :wrench: add declarations for highlightjs

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,6 +1,7 @@
 declare module "remark-slide";
 declare module "remark-slide/src/polyfills";
 declare module "remark-slide/src/remark/components/styler/styler";
+declare module "highlightjs-solidity";
 
 declare module "*.md" {
   const content: string;
